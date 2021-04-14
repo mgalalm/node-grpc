e4a2755d0d1392be724ee5d2d7214ed92718828d
mgalal2 <mgalal2@its.jnj.com>
2021-04-14 23:48:25 +0100

    Intial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+let grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+const server = new grpc.Server();
+const SERVER_ADDRESS = '0.0.0.0:9091';
+
+let proto = grpc.loadPackageDefinition(
+  protoLoader.loadSync('protos/chat.proto', {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  })
+);
+
+let users = [];
+
+function join(call, callback) {
+  users.push(call);
+  notifyChat({ user: 'Server', text: 'new user joined ...' });
+}
+
+function send(call, callback) {
+  notifyChat(call.request);
+}
+
+function notifyChat(message) {
+  users.forEach((user) => {
+    user.write(message);
+  });
+}
+
+server.addService(proto.chat.Chat.service, { join: join, send: send });
+server.bind(SERVER_ADDRESS, grpc.ServerCredentials.createInsecure());
+server.start();
